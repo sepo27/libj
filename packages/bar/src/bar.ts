c81b404c524d1bdd94f76feb6c5e621f7ef9c0a2
Sergey Poskachey <seregynp@gmail.com>
2020-09-08 21:28:47 +0400

    Update bar

--- packages/bar/src/bar.ts
+++ packages/bar/src/bar.ts
@@ -1,4 +1,4 @@
 import { baz } from './baz';
 import { foo } from '../../foo/src';
 
-export const bar = () => console.log('bar', baz(), foo());
+export const bar = () => console.log('barrrr', baz(), foo());
