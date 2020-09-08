ab8c77123786c541152b264812d4d8ae7a26d91c
Sergey Poskachey <seregynp@gmail.com>
2020-09-08 22:02:30 +0400

    Update prepublishOnly.sh

--- scripts/prepublishOnly.sh
+++ scripts/prepublishOnly.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "[prepublishOnl] info: Commiting changes"
+if [ -n "$(git status --porcelain)" ]; then
+  echo "there are changes";
+else
+  echo "no changes";
+fi
 
-git commit -a -m "Commiting after repack"
+#echo "[prepublishOnl] info: Commiting changes"
+
+#git commit -a -m "Commiting after repack"
