3bf2c593843c0972e88dbbe74371f2aed616a3af
Sergey Poskachey <seregynp@gmail.com>
2020-09-18 20:44:54 +0400

    Clean up

--- scripts/prepublishOnly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -n "$(git status --porcelain)" ]; then
-  echo "[prepublishOnly] info: Commiting prepublish changes"
-  git commit -a -m "Prepublish changes"
-else
-  echo "[prepublishOnly] info: Nothing to commit for prepublish";
-fi
