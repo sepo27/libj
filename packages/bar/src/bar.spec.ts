69aa0b3513f9ee0d35adb96120b44cc0a402beb3
Sergey Poskachey <seregynp@gmail.com>
2020-09-20 21:46:24 +0400

    #7: Setup precommit hook

---
+++ packages/bar/src/bar.spec.ts
@@ -0,0 +1,5 @@
+describe('bar', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
--- packages/bar/src/bar.ts
+++ packages/bar/src/bar.ts
@@ -1,4 +1,4 @@
 import { baz } from './baz';
 import { foo } from '../../foo/src';
 
-export const bar = () => console.log('barrrr', baz(), foo());
+export const bar = (): any => console.log('barrrr', baz(), foo());
