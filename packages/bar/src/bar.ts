982a8ebf11d3a7b5a250dcec48d6ac1972c6bd36
Sergey Poskachey <seregynp@gmail.com>
2021-01-08 00:23:40 +0400

    Test commit

--- packages/bar/src/bar.ts
+++ packages/bar/src/bar.ts
@@ -1,4 +1,4 @@
 import { baz } from './baz';
 import { foo } from '../../foo/src';
 
-export const bar = (): any => console.log('barr', baz(), foo());
+export const bar = (): any => console.log('bar', baz(), foo());
