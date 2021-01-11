6b0473f0c8527a59ea2ea26bc0cb65379a3c2b9d
Sergey Poskachey <seregynp@gmail.com>
2021-01-11 22:23:12 +0400

    Update dummy bar package

--- packages/bar/src/bar.ts
+++ packages/bar/src/bar.ts
@@ -2,6 +2,6 @@ import { baz } from './baz';
 import { foo } from '../../foo/src';
 
 export const bar = (): boolean => {
-  console.log('barrr', baz(), foo());
+  console.log('barr', baz(), foo());
   return true;
 };
