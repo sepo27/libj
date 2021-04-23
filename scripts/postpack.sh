2a621ae5ecaeffa24b804890f3207cab292409dc
Sepo <sepo@one.com>
2021-04-23 18:00:44 +0400

    routing: Try postpack update

--- scripts/postpack.sh
+++ scripts/postpack.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$(git status --porcelain)" ]; then
   echo "[postpack] info: Commiting postpack changes"
-  git commit -a -m "Postpack commit"
+  git commit -a -m "Postpack commit" --amend
 else
   echo "[postpack] info: Nothing to commit for postpack";
 fi
