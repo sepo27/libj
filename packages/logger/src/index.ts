c7b9ad111bf7d9388e53ca8034211d3bf1058508
Sepo <sepo@one.com>
2021-10-26 20:44:27 +0400

    logger: Added index

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,6 @@
+import { BaseLogger } from './base/BaseLogger';
+import { ConsoleLogger } from './console/ConsoleLogger';
+import { LoggerInterface } from './LoggerInterface';
+import { LoggerLevel } from './LoggerLevel';
+
+export { BaseLogger, ConsoleLogger, LoggerInterface, LoggerLevel };
