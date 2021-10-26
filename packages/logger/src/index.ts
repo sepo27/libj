9712fae09eec933082e882772175546351a1ee53
Sepo <sepo@one.com>
2021-10-26 20:52:57 +0400

    logger: Update index

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,6 +1,6 @@
 import { BaseLogger } from './base/BaseLogger';
 import { ConsoleLogger } from './console/ConsoleLogger';
-import { LoggerInterface } from './LoggerInterface';
+import { LoggerInterface, LoggerArg } from './LoggerInterface';
 import { LoggerLevel } from './LoggerLevel';
 
-export { BaseLogger, ConsoleLogger, LoggerInterface, LoggerLevel };
+export { BaseLogger, ConsoleLogger, LoggerInterface, LoggerArg, LoggerLevel };
