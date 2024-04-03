7455b7bff21be28b3984831fca167fdec18850eb
Sepo <sepo@one.com>
2024-04-03 11:42:15 +0400

    exec: Export more docker types

--- packages/exec/src/lib/exec/docker/index.ts
+++ packages/exec/src/lib/exec/docker/index.ts
@@ -1,6 +1,6 @@
 export { dockerExec } from './dockerExec';
 export { dockerExecExec } from './exec/dockerExecExec';
 export { parseDockerExecExecParams } from './exec/parseDockerExecExecParams';
-export { DockerExecExecParams } from './exec/types';
+export { DockerExecExecParams, ListDockerExecExecParams, PartialStructDockerExecExecParams } from './exec/types';
 
 export { dockerComposeExec } from './dockerComposeExec';
