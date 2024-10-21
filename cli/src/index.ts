2af8f74b3a3714c79463c969c1d6e77788e00ea9
Sepo <sepo@one.com>
2024-10-21 14:33:35 +0400

    Fix project cli ts

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,6 +1,7 @@
 import { program as cli } from 'commander';
 import { bundlePackageCommand } from './commands/bundlePackage/bundlePackageCommand';
 
+// @ts-ignore: TODO
 cli.addCommand(bundlePackageCommand);
 
 cli.parse(process.argv);
